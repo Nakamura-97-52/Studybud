4d95157c34ca90a4aff07e7092092f61e1860451
RY <n.ryo.sho.0@gmail.com>
2022-12-25 12:16:46 +0900

    Fix errors

--- studybud/settings.py
+++ studybud/settings.py
@@ -135,4 +135,4 @@ MEDIA_ROOT = BASE_DIR / 'static/image'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-djanfo_heroku.settings(locals())
+django_heroku.settings(locals())
